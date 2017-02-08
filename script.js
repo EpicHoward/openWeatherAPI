2fc9e8aff3964e8e4d79eecf6c153de630ca7769
Eric R. Howard <ehoward@hudsonhs.com>
2017-02-08 23:07:25 +0000

    update

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
         $("input").val();
 
         var city = $("input").val();
-        $.get("http://api.openweathermap.org/data/2.5/weather?q=nyci&units=imperial&appid=21b0a78e60bc539c02c647c6f01ebfba")
 
         var url = "http://api.openweathermap.org/data/2.5/weather?q=" +
             city +
@@ -14,15 +13,16 @@ $(document).ready(function() {
 
         $.get(url, function(data) {
 
-            alert(data.main.temp)
-            
-            $("#weather-results").append(data.min.temp)
-            
-            $("#temp").append(data.main.temp);
-            
-            $("#hum").append(data.main.humidity);
-            
-            $("#wind").append(data.wind.speed);
+            $("#temp").html(data.main.temp);
+
+            $("#hum").html(data.main.humidity);
+
+            $("#wind-Speed").html(data.wind.speed);
+
+$("#weather-results").append("<img src=\"https://api.mapbox.com/v4/mapbox.dark/"+data.coord.ion+","+
+data.coord.lat + 
+", 12/600x600.png?access_token=21b0a78e60bc539c02c647c6f01ebfba\">");
+
 
         });
     });
@@ -30,3 +30,6 @@ $(document).ready(function() {
 
 
 });
+
+
+
